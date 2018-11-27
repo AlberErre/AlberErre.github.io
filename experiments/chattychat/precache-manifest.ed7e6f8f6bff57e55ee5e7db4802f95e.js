fec22596990d40f0a9fadb287a85cabbc7fdc87b
Alber Erre <arblugo@gmail.com>
2018-11-27 00:14:08 +0000

    redux-webpack-build (chattychat)

---
+++ experiments/chattychat/precache-manifest.ed7e6f8f6bff57e55ee5e7db4802f95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db4249d80c57a973d62ac45c8c051cd1",
+    "url": "/static/media/chattyChat.db4249d8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c620a75f92eedc34c8f",
+    "url": "/static/js/main.4c620a75.chunk.js"
+  },
+  {
+    "revision": "cb943a5055d8a1a67aed",
+    "url": "/static/js/1.cb943a50.chunk.js"
+  },
+  {
+    "revision": "4c620a75f92eedc34c8f",
+    "url": "/static/css/main.01c518f8.chunk.css"
+  },
+  {
+    "revision": "64dd5ac38afffd491266f5d4cd586ca7",
+    "url": "/index.html"
+  }
+];
